9274315a8882a26e41e6040a503c52968d92d82f
tenseoverflow <contact@hen.ee>
2024-08-22 17:52:36 +0300

    feat: sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,16 @@
 </script>
 
 <main>
-    <div class="bg-gradient-to-r from-green-500 to-green-300">
+    <div class="bg-gradient-to-r from-green-500 to-green-300 lg:h-screen">
         <nav class="navbar flex-wrap gap-x-28 bg-transparent px-12 pt-12 lg:px-40">
             <div class="flex-1">
                 <a class="btn btn-ghost text-2xl dark:text-black" href="/">
                     <!-- <img src="" alt="Logo" width="112" height="28"> -->
-                    Delyver
+                    Delyver.ai
                 </a>
             </div>
             <div class="navbar-menu">
-                <div class="flex-none">
+                <div class="flex lg:flex-row flex-wrap gap-2">
                     <a
                         class="navbar-item btn btn-ghost text-2xl dark:text-black"
                         href="#why"
@@ -26,11 +26,10 @@
                 </div>
             </div>
         </nav>
-        <div class="px-8 py-10 lg:py-28 lg:px-40">
-            <div class="hero-content flex-col lg:flex-row">
+        <div class="px-8 py-10 lg:py-24 lg:pl-40">
+            <div class="hero-content flex-col lg:flex-row justify-center">
 
-            <div class="z-0 max-w-5xl ">
-
+            <div class="z-0 max-w-5xl">
             <h1 class="text-3xl lg:text-6xl font-bold dark:text-black">Streamline Your Supply Chain with Delyver.ai
 </h1>
             <p class="py-6 text-xl dark:text-black">
@@ -40,9 +39,10 @@
                 <input class="input join-item" placeholder="Let's get in contact" type="email">
                 <button class="btn join-item">Talk to us</button>
             </div> -->
+            <a href="#" class="btn btn-primary bg-slate-100 hover:bg-slate-300 border-0 text-xl">Learn more</a>
             <a
                 href="#contact"
-                class="btn btn-primary border-0 bg-slate-900 text-xl text-white hover:bg-black"
+                class="btn btn-primary border-0 bg-slate-900 text-xl text-white hover:bg-slate-950"
                 use:scrollTo={'contact'}
                 >Get in touch</a
             >
@@ -60,9 +60,19 @@
         </div>
     </div>
 
+    <div class="flex flex-col items-center gap-10 px-16 py-20 text-center lg:px-72">
+        <h2 class="m-auto text-3xl font-bold">Expand Your Reach, Shrink Your Impact</h2>
+        <p class="m-auto text-xl">Global expansion doesn’t have to come at the expense of the environment. With Delyver.ai, you can grow your business across borders while keeping your environmental footprint to a minimum. Our AI-driven logistics solutions optimize every aspect of your supply chain, allowing you to receive your products in the most efficient and eco-friendly way possible.</p>
+    </div>
+
+    <div class="flex flex-col items-center gap-10 px-16 py-20 text-center lg:px-72">
+        <h2 class="m-auto text-3xl font-bold">How it works</h2>
+        <p class="m-auto text-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus placeat illum obcaecati autem qui minima labore tempore perferendis earum odit rerum explicabo consequuntur tempora, consectetur cumque dignissimos ipsum eum quam?</p>
+    </div>
+
     <div
         id="why"
-        class="flex flex-col flex-wrap items-center gap-10 px-16 py-20 text-center lg:px-72"
+        class="flex flex-col flex-wrap items-center gap-10 px-16 py-20 lg:px-72"
         use:scrollRef={'why'}
     >
         <h2 class="m-auto text-5xl font-bold">Why choose us</h2>
@@ -114,36 +124,34 @@ By leveraging our AI-driven approach, we help you save up to 60% on your logisti
             </div>
         </div>
 
-        <div id="numbers" class="flex w-full flex-col flex-wrap items-center gap-8">
-            <h3 class="m-auto text-3xl font-bold">Numbers</h3>
-            <div class="w-full">
-                <h4 class="w-full text-5xl text-primary">100%</h4>
-                <p class="my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            </div>
-            <div class="w-full">
-                <h4 class="w-full text-5xl text-primary">100%</h4>
-                <p class="my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            </div>
-            <div class="w-full">
-                <h4 class="w-full text-5xl text-primary">100%</h4>
-                <p class="my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            </div>
-        </div>
     </div>
 
     <div class=" bg-slate-900">
-        <div use:scrollRef={'contact'} id="contact" class="px-16 py-20 lg:px-72">
+        <div use:scrollRef={'contact'} id="contact" class="px-16 py-20 lg:px-96">
             <h2 class="py-3 text-center text-5xl font-bold text-white">Contact us</h2>
             <div class="text-center">
                 <p class="py-6 text-xl text-white">Let's take your logistics to the next level!</p>
-                <a href="mailto:example@example.com" class="link text-white">example@example.com</a>
+                <form action="" method="get" class="flex flex-col gap-3">
+                    <label class="input input-bordered flex items-center gap-2">
+                      Name
+                      <input type="text" class="grow" placeholder="Delyver.ai" />
+                    </label>
+                    <label class="input input-bordered flex items-center gap-2">
+                      Email
+                      <input type="text" class="grow" placeholder="delyver@delyver.ai" />
+                    </label>
+                    <textarea class="textarea textarea-md textarea-bordered" placeholder="Message" />
+                    <button class="btn btn-primary border-0" type="submit">Send</button>
+                </form>
             </div>
         </div>
         <footer>
             <div class="px-8 pb-16 lg:px-16">
                 <div class="flex flex-auto flex-wrap justify-between gap-2">
-                    <p class="text-white">Delyver. All rights reserved.</p>
-                    <p class="self-end text-right text-white">2024</p>
+                    <p class="text-white">Delyver. All rights reserved. 2024</p>
+                    <p class="self-end text-right text-white">
+                <a href="mailto:example@example.com" class="link text-white">example@example.com</a>
+                </p>
                 </div>
             </div>
         </footer>
